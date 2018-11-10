c1d00d7c0ebf45077a66cb6f34093cb31f99fb0f
dwdy250 <timdowd1984@gmail.com>
2018-11-10 20:12:29 +0000

    currently dealing with zipcode formatting

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,3 +1,13 @@
-puts "EventManager Initialized!"
-contents = File.read "event_attendees.csv"
-puts contents
+require "csv"
+
+puts "EventManager initialized."
+
+
+
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
+end
