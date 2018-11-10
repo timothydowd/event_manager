0892af9d4a030d2d7811a34a8fb3cef7f23c64cf
dwdy250 <timdowd1984@gmail.com>
2018-11-10 23:20:07 +0000

    moving on to google api

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,13 +1,18 @@
-require "csv"
+require 'csv'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
 
 puts "EventManager initialized."
 
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
 
-
-
-contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
   name = row[:first_name]
-  zipcode = row[:zipcode]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
   puts "#{name} #{zipcode}"
 end
