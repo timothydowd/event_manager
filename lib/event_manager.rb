042ffb5c1ffba0376d413e6d43a03d595bca585f
dwdy250 <timdowd1984@gmail.com>
2018-11-11 20:52:15 +0000

    event manager now creats multiple forms in output directory.  Finished

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -21,6 +21,16 @@ def legislators_by_zipcode(zip)
   end
 end
 
+def save_thank_you_letters(id,form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
+
 puts "EventManager initialized."
 
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
@@ -29,12 +39,12 @@ template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
-  puts form_letter
+
+  save_thank_you_letters(id,form_letter)
 end
